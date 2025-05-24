f4acc8d365de14e1ac4380d4855f9c2df22c9754
paulorobertorodrigues <pauloroberto23061985@gmail.com>
2025-05-24 13:54:07 -0300

    incluindo para salvar pdf e visualizar pelo QRcode

--- src/Components/PDFPage.tsx
+++ src/Components/PDFPage.tsx
@@ -1,233 +1,105 @@
-// // import React from 'react';
-// import { useLocation, useParams } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './Certificado';
-
-// import QRCode from "react-qr-code";
-
-// import html2pdf from 'html2pdf.js';
-// // import { QRCodeCanvas } from 'qrcode.react';
-// // import { QRCode } from 'qrcode.react';
-// import logoG8 from '../Images/logo_g8.png';
-// import certifiedLogo from '../Images/certified.png';
-
-// function PDFPage() {
-//   const location = useLocation();
-//   const { id } = useParams(); // Captura o id ou codigo da URL
-//   const { data, revisao, codigo, validade, responsavel, registro } = location.state || {};
-
-//   // const generatePDF = () => {
-//   //   const element = document.getElementById('todo');
-    
-//   //   const button = document.getElementById('pdf-button');
-//   //   button.style.display = 'none'; // Esconde o botão durante a geração do PDF
-
-//   //   html2pdf()
-//   //     .from(element)
-//   //     .set({
-//   //       margin: 1,
-//   //       filename: `certificado-${codigo || id}.pdf`,
-//   //       image: { type: 'jpeg', quality: 0.98 },
-//   //       html2canvas: { scale: 2 },
-//   //       jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
-//   //     })
-//   //     .save()
-//   //     .finally(() => {
-//   //       button.style.display = 'block'; // Mostra o botão novamente após a geração
-//   //     });
-//   // };
-
-
-
-//   const generatePDF = () => {
-//     const element = document.getElementById('todo');
-    
-//     const button = document.getElementById('pdf-button');
-//     if (button) { // Verifica se o botão não é null
-//       button.style.display = 'none'; // Esconde o botão durante a geração do PDF
-//     }
-  
-//     html2pdf()
-//       .from(element)
-//       .set({
-//         margin: 1,
-//         filename: `certificado-${codigo || id}.pdf`,
-//         image: { type: 'jpeg', quality: 0.98 },
-//         html2canvas: { scale: 2 },
-//         jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
-//       })
-//       .save()
-//       .finally(() => {
-//         if (button) { // Verifica novamente se o botão existe
-//           button.style.display = 'block'; // Mostra o botão novamente após a geração
-//         }
-//       });
-//   };
-  
-
-
-
-
-
-//   const currentUrl = window.location.href; // URL atual da página com o ID
-
-//   return (
-//     <div className="container" id="todo">
-//       <div className="card text-center">
-//         <div className="card-header d-flex justify-content-between align-items-center">
-
-//           <img src={logoG8} width="200" height="100" alt="Certified Logo" />
-
-//           <QRCode value={currentUrl} size={100} className="float-right" /> {/* QR code para a URL atual */}
-//         </div>
-//         <div className="d-flex justify-content-center m-4">
-//           <h2><i>Certificado de Autenticidade</i></h2>
-//         </div>
-//         <div className="container text-center">
-//           <div className="row row-cols-2">
-//             <div className="col">
-//               <p>Data da Elaboração do Documento <br /><strong>{new Date(data).toLocaleDateString('pt-BR')}</strong></p>
-//             </div>
-//             <div className="col">
-//               <p>Revisão do Documento <br /><strong>{revisao}</strong></p>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="container text-center">
-//           <div className="row row-cols-2">
-//             <div className="col">
-//               <p>Código do Documento<br /><strong>{codigo}</strong></p>
-//             </div>
-//             <div className="col">
-//               <p>Validade<br /><strong>{new Date(validade).toLocaleDateString('pt-BR')}</strong></p>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="container text-center">
-//           <div className="row row-cols-2">
-//             <div className="col">
-//               <p>Responsável Técnico<br /><strong>{responsavel}</strong></p>
-//             </div>
-//             <div className="col">
-//               <p>Registro Profissional<br /><strong>CREA: {registro}</strong></p>
-//             </div>
-//           </div>
-//         </div><br/><br/>
-
-//         <div className="container custom-container">
-//           <div className="row row-cols-2">
-//             <div className="col custom-margin">
-//               <p className="mb-0"><strong>G8 Consultoria, Assessoria & Treinamento</strong></p>
-//               <div className="cnpj">
-//                 <p className="mb-0">CNPJ: 17.819.089/000115</p>
-//                 <p className="mb-0">Web-site: <a href="https://www.g8online.com.br" className="stretched-link" target="_blank">https://www.g8online.com.br</a></p>
-//                 <p className="mb-0">E-mail: <a href="mailto:g8@g8online.com.br" className="stretched-link" target="_blank">g8@g8online.com.br</a></p>
-//               </div>
-//             </div>
-//             <div className="col" id="logo-certified">
-//               <img src={certifiedLogo} width="130" height="130" alt="Certified Logo" />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <button id="pdf-button" onClick={generatePDF} className="btn btn-primary mt-4">Baixar PDF</button>
-//     </div>
-//   );
-// }
-
-// export default PDFPage;
-
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+// import React from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Certificado';
 
 import QRCode from "react-qr-code";
-import html2pdf from 'html2pdf.js';
-import axios from 'axios';
 
+import html2pdf from 'html2pdf.js';
+// import { QRCodeCanvas } from 'qrcode.react';
+// import { QRCode } from 'qrcode.react';
 import logoG8 from '../Images/logo_g8.png';
 import certifiedLogo from '../Images/certified.png';
 
-// Interface para o objeto certificado
-interface Certificado {
-  data: string;
-  revisao: string;
-  codigo: string;
-  validade: string;
-  responsavel: string;
-  registro: string;
-}
-
 function PDFPage() {
-  // Tipando useParams para que id seja string | undefined
-  const { id } = useParams<{ id: string }>();
-  
-  // Estado com tipo Certificado ou null (inicialmente null)
-  const [certificado, setCertificado] = useState<Certificado | null>(null);
+  const location = useLocation();
+  const { id } = useParams(); // Captura o id ou codigo da URL
+  const { data, revisao, codigo, validade, responsavel, registro } = location.state || {};
 
-  // Buscar dados do certificado com base no código
-  useEffect(() => {
-    if (!id) return; // Proteção caso id seja undefined
+  // const generatePDF = () => {
+  //   const element = document.getElementById('todo');
+    
+  //   const button = document.getElementById('pdf-button');
+  //   button.style.display = 'none'; // Esconde o botão durante a geração do PDF
 
-    const fetchCertificado = async () => {
-      try {
-        const res = await axios.get<Certificado>(`${import.meta.env.VITE_API_URL}/certificado/codigo/${id}`);
-        setCertificado(res.data);
-      } catch (err) {
-        console.error("Erro ao buscar certificado:", err);
-      }
-    };
+  //   html2pdf()
+  //     .from(element)
+  //     .set({
+  //       margin: 1,
+  //       filename: `certificado-${codigo || id}.pdf`,
+  //       image: { type: 'jpeg', quality: 0.98 },
+  //       html2canvas: { scale: 2 },
+  //       jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+  //     })
+  //     .save()
+  //     .finally(() => {
+  //       button.style.display = 'block'; // Mostra o botão novamente após a geração
+  //     });
+  // };
 
-    fetchCertificado();
-  }, [id]);
+
 
   const generatePDF = () => {
     const element = document.getElementById('todo');
+    
     const button = document.getElementById('pdf-button');
-    if (button) button.style.display = 'none';
-
+    if (button) { // Verifica se o botão não é null
+      button.style.display = 'none'; // Esconde o botão durante a geração do PDF
+    }
+  
     html2pdf()
-      .from(element!)
+      .from(element)
       .set({
         margin: 1,
-        filename: `certificado-${certificado?.codigo || id}.pdf`,
+        filename: `certificado-${codigo || id}.pdf`,
         image: { type: 'jpeg', quality: 0.98 },
         html2canvas: { scale: 2 },
         jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
       })
       .save()
       .finally(() => {
-        if (button) button.style.display = 'block';
+        if (button) { // Verifica novamente se o botão existe
+          button.style.display = 'block'; // Mostra o botão novamente após a geração
+        }
       });
   };
+  
 
-  if (!certificado) {
-    return <div className="container mt-5 text-center">Carregando certificado...</div>;
-  }
 
-  const qrCodeURL = `${window.location.origin}/certificado/${certificado.codigo}`;
+
+
+
+  const currentUrl = window.location.href; // URL atual da página com o ID
 
   return (
     <div className="container" id="todo">
       <div className="card text-center">
         <div className="card-header d-flex justify-content-between align-items-center">
-          <img src={logoG8} width="200" height="100" alt="Logo G8" />
-          <QRCode value={qrCodeURL} size={100} className="float-right" />
-        </div>
 
+          <img src={logoG8} width="200" height="100" alt="Certified Logo" />
+
+          <QRCode value={currentUrl} size={100} className="float-right" /> {/* QR code para a URL atual */}
+        </div>
         <div className="d-flex justify-content-center m-4">
           <h2><i>Certificado de Autenticidade</i></h2>
         </div>
-
         <div className="container text-center">
           <div className="row row-cols-2">
             <div className="col">
-              <p>Data da Elaboração do Documento<br /><strong>{new Date(certificado.data).toLocaleDateString('pt-BR')}</strong></p>
+              <p>Data da Elaboração do Documento <br /><strong>{new Date(data).toLocaleDateString('pt-BR')}</strong></p>
             </div>
             <div className="col">
-              <p>Revisão do Documento<br /><strong>{certificado.revisao}</strong></p>
+              <p>Revisão do Documento <br /><strong>{revisao}</strong></p>
+            </div>
+          </div>
+        </div>
+        <div className="container text-center">
+          <div className="row row-cols-2">
+            <div className="col">
+              <p>Código do Documento<br /><strong>{codigo}</strong></p>
+            </div>
+            <div className="col">
+              <p>Validade<br /><strong>{new Date(validade).toLocaleDateString('pt-BR')}</strong></p>
             </div>
           </div>
         </div>
@@ -235,39 +107,22 @@ function PDFPage() {
         <div className="container text-center">
           <div className="row row-cols-2">
             <div className="col">
-              <p>Código do Documento<br /><strong>{certificado.codigo}</strong></p>
+              <p>Responsável Técnico<br /><strong>{responsavel}</strong></p>
             </div>
             <div className="col">
-              <p>Validade<br /><strong>{new Date(certificado.validade).toLocaleDateString('pt-BR')}</strong></p>
+              <p>Registro Profissional<br /><strong>CREA: {registro}</strong></p>
             </div>
           </div>
-        </div>
-
-        <div className="container text-center">
-          <div className="row row-cols-2">
-            <div className="col">
-              <p>Responsável Técnico<br /><strong>{certificado.responsavel}</strong></p>
-            </div>
-            <div className="col">
-              <p>Registro Profissional<br /><strong>CREA: {certificado.registro}</strong></p>
-            </div>
-          </div>
-        </div>
-
-        <br /><br />
+        </div><br/><br/>
 
         <div className="container custom-container">
           <div className="row row-cols-2">
             <div className="col custom-margin">
               <p className="mb-0"><strong>G8 Consultoria, Assessoria & Treinamento</strong></p>
               <div className="cnpj">
-                <p className="mb-0">CNPJ: 17.819.089/0001-15</p>
-                <p className="mb-0">
-                  Web-site: <a href="https://www.g8online.com.br" className="stretched-link" target="_blank" rel="noopener noreferrer">https://www.g8online.com.br</a>
-                </p>
-                <p className="mb-0">
-                  E-mail: <a href="mailto:g8@g8online.com.br" className="stretched-link" target="_blank" rel="noopener noreferrer">g8@g8online.com.br</a>
-                </p>
+                <p className="mb-0">CNPJ: 17.819.089/000115</p>
+                <p className="mb-0">Web-site: <a href="https://www.g8online.com.br" className="stretched-link" target="_blank">https://www.g8online.com.br</a></p>
+                <p className="mb-0">E-mail: <a href="mailto:g8@g8online.com.br" className="stretched-link" target="_blank">g8@g8online.com.br</a></p>
               </div>
             </div>
             <div className="col" id="logo-certified">
@@ -276,7 +131,6 @@ function PDFPage() {
           </div>
         </div>
       </div>
-
       <button id="pdf-button" onClick={generatePDF} className="btn btn-primary mt-4">Baixar PDF</button>
     </div>
   );
