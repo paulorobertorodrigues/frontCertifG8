1cffc4b85ace1a337d521996e047ea468428c4ab
paulorobertorodrigues <pauloroberto23061985@gmail.com>
2025-05-24 14:20:48 -0300

    incluindo para salvar pdf e visualizar pelo QRcode

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,22 @@
-// import './App.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import Login from './Components/Login'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import Certificado from './Components/Certificado'
-// import PDFPage from  './Components/PDFPage'
-
-// function App() {
-
-//   return (
-
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path='/' element={<Login />}></Route>
-//         <Route path='/certificado' element={<Certificado />}></Route>
-//         <Route path="/pdf/:codigo" element={<PDFPage />}></Route>
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
-// export default App
-
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './Components/Login';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Certificado from './Components/Certificado';
-import PDFPage from './Components/PDFPage';
-import QRCodeReader from './Components/QRCodeReader'; // caminho corrigido
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './Components/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Certificado from './Components/Certificado'
+import PDFPage from  './Components/PDFPage'
 
 function App() {
+
   return (
+
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/certificado' element={<Certificado />} />
-        <Route path='/pdf/:codigo' element={<PDFPage />} />
-        <Route path='/scanner' element={<QRCodeReader />} /> {/* Nova rota para escanear */}
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/certificado' element={<Certificado />}></Route>
+        <Route path="/pdf/:codigo" element={<PDFPage />}></Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
