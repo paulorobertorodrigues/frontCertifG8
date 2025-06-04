abcac3779a2f322ac80faa87fbbcc5d53b774919
paulorobertorodrigues <pauloroberto23061985@gmail.com>
2025-06-03 21:40:23 -0300

    incluindo para salvar pdf e visualizar pelo QRcode

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import Login from './Components/Login'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Certificado from './Components/Certificado'
 import PDFPage from  './Components/PDFPage'
-//adicionado
-import QrCodeCertificado from './Components/QrCodeCertificado';
-//
+
 function App() {
 
   return (
@@ -16,9 +14,6 @@ function App() {
         <Route path='/' element={<Login />}></Route>
         <Route path='/certificado' element={<Certificado />}></Route>
         <Route path="/pdf/:codigo" element={<PDFPage />}></Route>
-
-        <Route path="/" element={<PDFPage />} />
-        <Route path="/qrcode/:id" element={<QrCodeCertificado />} />
       </Routes>
     </BrowserRouter>
   )
